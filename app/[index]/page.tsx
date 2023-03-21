fc4e6abdd5a5f64fb5f8cca7666fcb46e0a94d0c
Jagger <634750802@qq.com>
2023-03-21 16:53:24 +0800

    add links

--- app/[index]/page.tsx
+++ app/[index]/page.tsx
@@ -3,8 +3,13 @@ import IndustryDistribution from '@/charts/IndustryDistribution';
 import IndexCompositions from '@/charts/IndexCompositions';
 import IndexCompositionCountryDistribution from '@/charts/IndexCompositionCountryDistribution';
 import IndexCompositionExchangeDistribution from '@/charts/IndexCompositionExchangeDistribution';
+import { notFound } from 'next/navigation';
+import Link from 'next/link';
 
 export default function Home ({ params: { index } }: { params: { index: string } }) {
+  if (index !== 'SP500') {
+    notFound();
+  }
 
   return (
     <main className="max-w-[960px] mx-auto">
@@ -14,33 +19,33 @@ export default function Home ({ params: { index } }: { params: { index: string }
         This is a sample stock analysis application. We have selected S&P 500 Index data and detailed stock price changes for constituent stocks from 2013.1 to 2023.1 for this sample program. Based on this sample program, you can learn how to build a stock analysis program on TiDB Serverless and Data API. We will provide you with the sample code and dataset of this application for reference.
         Here, you can get:
       </p>
-      <ul className="text-lg text-primary list-disc list-inside mt-2">
-        <li>S&P 500 Index</li>
-        <li>Customized list of stocks, monitoring the stock price changes of selected constituent stocks</li>
-        <li>Ranking based on industry total market capitalization</li>
-        <li>Rankings of constituent stocks based on market capitalization in the industry</li>
-        <li>Distribution of constituent stocks by country (geography)</li>
-        <li>Proportional distribution of exchanges</li>
+      <ul className="basic-links text-lg text-primary list-disc list-inside mt-2">
+        <li><a href="#index">S&P 500 Index</a></li>
+        <li><Link href="/optional">Customized list of stocks, monitoring the stock price changes of selected constituent stocks</Link></li>
+        <li><a href="#industry-ranking">Ranking based on industry total market capitalization</a></li>
+        <li><a href='#compositions'>Rankings of constituent stocks based on market capitalization in the industry</a></li>
+        <li><a href='#geo'>Distribution of constituent stocks by country (geography)</a></li>
+        <li><a href='#exchanges'>Proportional distribution of exchanges</a></li>
         <li>Detailed information and Candle-stick chart of individual constituent stocks</li>
       </ul>
       <section className="mt-8">
-        <h2 className="my-4">
-          Overview
+        <h2 id="index" className="my-4">
+          S&P 500 Index
         </h2>
         <IndexOverview index={index} />
       </section>
       <section className="mt-8">
-        <h2 className="my-4">
+        <h2 id='compositions' className="my-4">
           Compositions
         </h2>
         <IndexCompositions index={index} />
-        <h3 className="mt-4">Country distribution</h3>
+        <h3 id='geo' className="mt-4">Country distribution</h3>
         <IndexCompositionCountryDistribution index={index} />
-        <h3 className="mt-4">Exchange distribution</h3>
+        <h3 id='exchanges' className="mt-4">Exchange distribution</h3>
         <IndexCompositionExchangeDistribution index={index} />
       </section>
       <section className="mt-8">
-        <h2 className="my-4">
+        <h2 id="industry-ranking" className="my-4">
           Sectors
         </h2>
         <IndustryDistribution index={index} />
