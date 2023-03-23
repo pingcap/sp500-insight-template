9cb8f8d014ff8ac101baaf26a1d19b24924d451e
Icemap <qizhi.wang@pingcap.com>
2023-03-23 12:00:44 +0800

    [Update] change /data-tracker API

--- app/data-tracker/route.ts
+++ app/data-tracker/route.ts
@@ -2,6 +2,11 @@ import { NextRequest, NextResponse } from 'next/server';
 import { getSymbols } from './dao';
 
 export async function GET (request: NextRequest) {
-    let symbols = await getSymbols();
+    let symbols = getSymbols()
+        .then(symbols => symbols)
+        .catch(err => {
+            console.error(err);
+            return NextResponse.error();
+        });
     return NextResponse.json(symbols);
 }
