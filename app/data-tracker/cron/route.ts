aac2303ebdba76120ed2bcd6ae7619573ffdffa1
Icemap <qizhi.wang@pingcap.com>
2023-03-23 18:58:24 +0800

    [Update] force-dynamic page

--- app/data-tracker/cron/route.ts
+++ app/data-tracker/cron/route.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from 'next/server';
 import { updateOldestSymbol } from '../dao/symbol';
 import { requestStockInfoAndSave } from '../[symbol]/sync-stock';
 
+export const dynamic = 'force-dynamic';
+
 // this function will be called by cron job
 // and the rate limit is 1 request per minute
 export async function GET (request: NextRequest) {
--- app/data-tracker/list/route.ts
+++ app/data-tracker/list/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getSymbols } from '../dao/symbol';
 
+export const dynamic = 'force-dynamic';
 export async function GET (request: NextRequest) {
     try {
         const symbols = await getSymbols();
