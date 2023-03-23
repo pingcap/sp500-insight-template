9a2434e39b72b59e5c522354c8b4d853666de9f1
Jagger <634750802@qq.com>
2023-03-23 14:09:02 +0800

    update home

--- charts/IndexCompositions.tsx
+++ charts/IndexCompositions.tsx
@@ -7,16 +7,16 @@ import { useTransform } from '@/utils/hook';
 import CommonToggleGroup from '@/components/ToggleGroup';
 
 const IndexCompositions: FC<{ index: string }> = ({ index }) => {
-  const [sector, setSector] = useState<string>();
+  const [sector, setSector] = useState<string>('All');
   const { data = [], isLoading } = useComposedEndpoint((sector) => {
-    if (sector) {
+    if (sector && sector !== 'All') {
       return [endpoints.index.compositions_by_sector.GET, { index_symbol: index, sector }];
     } else {
       return [endpoints.index.compositions.GET, { index_symbol: index }];
     }
   }, sector);
   const { data: rawSectors } = useEndpoint(endpoints.sectors.GET, {});
-  const sectors = useTransform(rawSectors, sectors => sectors?.map(s => ({ name: s.sector })) ?? []);
+  const sectors = useTransform(rawSectors, sectors => [{ name: 'All' }].concat(sectors?.map(s => ({ name: s.sector })) ?? []));
   return (
     <>
       <CommonToggleGroup
