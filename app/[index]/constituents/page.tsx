a811db89e991d5c2ca0caaf78b6256aa651425e0
winkyao <golangwink@gmail.com>
2023-03-24 21:09:59 +0800

    *: refine page copies

--- app/[index]/constituents/page.tsx
+++ app/[index]/constituents/page.tsx
@@ -8,13 +8,17 @@ export default function Page ({ params: { index } }: { params: { index: string }
       <h2 className="my-4">
         Constituents
       </h2>
-      <p className="mt-4">A constituent is a company whose shares are part of an index such as the S&P 500 or Dow Jones Industrial Average (DJIA). It is a component or a member of an index. The weighted aggregation of the share prices of all its constituents is used to calculate the value of an index.</p>
+      <p className="mt-4">The following list shows the basic information of all constituent stocks in the S&P 500, including the latest price in the dataset, 
+      the price change of the day, the percentage change, market capitalization, and revenue growth rate. You can filter the list by selecting different industry tags.</p>
       <IndexCompositions index={index} />
       <h3 id="geo" className="mt-4">Country & Region distribution</h3>
+      <p className="mt-4">The following map shows the geographic distribution of constituent stocks based on country. Hovering your mouse over the highlighted areas allows you to see the number of companies in that region. 
+      Although most of the S&P 500 constituent stocks are from the United States, this is a showcase of how to implement a geographic chart, which can serve as a reference.</p>
       <IndexCompositionGeoDistribution index={index} />
       <h3 id="exchanges" className="mt-4">Exchange distribution</h3>
-      <p className="mt-4">In stock market trading, an exchange is a marketplace where stocks, bonds, options, futures, and other financial instruments are bought and sold. Exchanges provide a platform for buyers and sellers to meet and negotiate prices. They also provide a mechanism for pricing and clearing trades, as well as collecting and distributing market data.</p>
+      <p className="mt-4">In stock market trading, an exchange refers to a marketplace where stocks, bonds, options, futures, and other financial instruments are bought and sold. 
+      The following pie chart illustrates the composition of S&P 500 constituent stocks across different exchanges. This is also an example showcasing how to implement a pie chart.</p>
       <IndexCompositionExchangeDistribution index={index} />
     </section>
   );
-}
+map
--- app/[index]/sector-ranking/page.tsx
+++ app/[index]/sector-ranking/page.tsx
@@ -5,7 +5,9 @@ export default function Page ({ params: { index } }: { params: { index: string }
     <section className="mt-8">
       <h2 id="industry-ranking">Ranking</h2>
       <p className="mt-4">
-        Based on Industry total capitalization
+      The following chart shows the industry ranking of S&P 500 constituent stocks based on market capitalization. 
+      In addition to market capitalization, we also display the number of companies and average revenue growth rate for each industry in the chart.<br>
+      we also provide a table format at the bottom, which is useful for a more intuitive view of the various industry metrics.
       </p>
       <SectorsRanking index={index} />
     </section>
