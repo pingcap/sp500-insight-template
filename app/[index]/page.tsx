29f3832d84c2cb43ef6e4e51e44edb63eed259cb
winkyao <golangwink@gmail.com>
2023-03-24 20:14:18 +0800

    index: refine page copies

--- app/[index]/page.tsx
+++ app/[index]/page.tsx
@@ -12,17 +12,18 @@ export default function Home ({ params: { index } }: { params: { index: string }
       <h1>S&P 500 Insight</h1>
       <h2 className="mt-4">Overview</h2>
       <p className="mt-4">
-        This is a sample stock analysis application. We have selected S&P 500 Index data and detailed stock price changes for constituent stocks from 2013.1 to 2023.1 for this sample program. Based on this sample program, you can learn how to build a stock analysis program on TiDB Serverless and Data API. We will provide you with the sample code and dataset of this application for reference.
-        Here, you can get:
+        This is a stock analysis demo that uses [TiDB Serverless](https://tidbcloud.com/) and [TiDB Cloud Data Service](https://docs.pingcap.com/tidbcloud/data-service-overview). 
+        We analyzed the data of the S&P 500 from 2013 to 2023 and used it as a dataset to build this application. You can get the dataset and code for this demo[Todo: please add link here], then you can rebuild the same application on your own TiDB Serverless Cluster for free.
+        In this demo, you can see the following features:
       </p>
       <ul className="basic-links text-lg text-primary list-disc list-inside mt-2">
         <li><a href="#index">S&P 500 Index</a></li>
-        <li><Link href="/optional">Customized list of stocks, monitoring the stock price changes of selected constituent stocks</Link></li>
-        <li><Link href="/SP500/sector-ranking">Ranking based on industry total market capitalization</Link></li>
+        <li><Link href="/optional">List of Selected Stocks</Link></li>
+        <li><Link href="/SP500/sector-ranking">Analysis of constituent stocks based on different industries</Link></li>
         <li><Link href="/SP500/constituents">Rankings of constituent stocks based on market capitalization in the industry</Link></li>
         <li><Link href="/SP500/constituents#geo">Distribution of constituent stocks by country (geography)</Link></li>
         <li><Link href="/SP500/constituents#exchanges">Proportional distribution of exchanges</Link></li>
-        <li>Detailed information and Candle-stick chart of individual constituent stocks</li>
+        <li>Detailed information of stocks, including candle-stick chart over the past decade</li>
       </ul>
       <section className="mt-8">
         <h2 id="index" className="my-4">
@@ -38,7 +39,7 @@ export default function Home ({ params: { index } }: { params: { index: string }
           <a href="https://en.wikipedia.org/wiki/Stock_exchange" target="_blank">stock exchanges</a>
           {' '}
           in the United States. It is one of the most commonly followed equity indices. You can view the
-          changes in the S&P 500 Index by selecting a time frame ranging from 7 days (7D) to MAX.
+          changes in the following S&P 500 Index by selecting a time frame ranging from 7 days (7D) to MAX.
         </p>
         <IndexOverview index={index} />
       </section>
