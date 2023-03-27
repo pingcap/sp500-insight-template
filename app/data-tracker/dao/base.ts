31f9ecb800226328369b3f72eb71ff5acabc4a58
Jagger <634750802@qq.com>
2023-03-27 15:15:10 +0800

    refine connections

--- app/data-tracker/dao/base.ts
+++ app/data-tracker/dao/base.ts
@@ -1,21 +1,38 @@
-import {createConnection, Connection, RowDataPacket} from 'mysql2/promise';
+import { Connection, createConnection } from 'mysql2/promise';
 
 export interface Stock {
-    symbol: string;
-    time: string;
-    high: number;
-    low: number;
-    open: number;
-    close: number;
-    volume: number;
+  symbol: string;
+  time: string;
+  high: number;
+  low: number;
+  open: number;
+  close: number;
+  volume: number;
 }
 
 export interface Symbol {
-    symbol: string;
-    createdDate: Date;
-    updatedDate: Date;
+  symbol: string;
+  createdDate: Date;
+  updatedDate: Date;
 }
 
-export function getConnection(): Promise<Connection> {
-    return createConnection(process.env.DATABASE_URL || "mysql://root:@localhost:4000/sp500insight");
+function getConnection (): Promise<Connection> {
+  return createConnection(process.env.DATABASE_URL || 'mysql://root:@localhost:4000/sp500insight');
+}
+
+export async function withAutoreleaseConnection<T> (run: (conn: Connection) => Promise<T>, hasTransaction = false): Promise<T> {
+  const conn = await getConnection();
+  try {
+    return await run(conn);
+  } catch (e) {
+    if (hasTransaction) {
+      await conn.rollback();
+    }
+    throw e;
+  } finally {
+    if (hasTransaction) {
+      await conn.commit();
+    }
+    conn.destroy();
+  }
 }
--- app/data-tracker/dao/symbol.ts
+++ app/data-tracker/dao/symbol.ts
@@ -1,122 +1,95 @@
+import { Connection, RowDataPacket } from 'mysql2/promise';
+import { Symbol, withAutoreleaseConnection } from './base';
 
-import {RowDataPacket} from 'mysql2/promise';
-import {getConnection, Symbol} from './base';
+export function getSymbols (): Promise<Symbol[]> {
+  return withAutoreleaseConnection(async connection => {
+    const results = await connection.query(
+      `SELECT stock_symbol, created_date, updated_date
+       FROM user_selected_symbol
+       ORDER BY created_date`,
+    );
+    let rows: RowDataPacket[] = (results as RowDataPacket[][])[0];
+    return rows.map(row => ({
+        symbol: row.stock_symbol,
+        createdDate: new Date(Date.parse(row.created_date)),
+        updatedDate: new Date(Date.parse(row.updated_date)),
+      }),
+    );
+  });
 
-export function getSymbols(): Promise<Symbol[]> {
-    return new Promise((resolve, reject) => {
-      getConnection().then(connection => {
-        connection.query(
-            `SELECT stock_symbol, created_date, updated_date
-            FROM user_selected_symbol
-            ORDER BY created_date`
-        ).catch(err => {
-          console.error(err);
-          reject(err);
-        }).then((results) => {
-          let rows: RowDataPacket[] = (results as RowDataPacket[][])[0];
-          let symbols = rows.map(row => ({
-              symbol: row.stock_symbol,
-              createdDate: new Date(Date.parse(row.created_date)),
-              updatedDate: new Date(Date.parse(row.updated_date))
-            })
-          );
-          resolve(symbols);
-        });
-      })
-    });
 }
 
-export function addSymbol(symbol: string) {
-    getConnection().then(connection => {
-        connection.query(
-            `INSERT INTO user_selected_symbol (stock_symbol, created_date, updated_date) 
-            VALUES (?, NOW(), NOW())`, [symbol]
-        );
-    });
+export async function addSymbol (symbol: string) {
+  await withAutoreleaseConnection(async connection => {
+    await connection.query(
+      `INSERT INTO user_selected_symbol (stock_symbol, created_date, updated_date)
+       VALUES (?, NOW(), NOW())`, [symbol],
+    );
+  });
 }
 
-export function deleteSymbol(symbol: string) {
-    getConnection().then(connection => {
-        connection.query(`DELETE FROM user_selected_symbol WHERE stock_symbol = ?`, [symbol])
-        .then(() => {
-            connection.query(`DELETE FROM user_selected_stocks WHERE symbol = ?`, [symbol]);
-        })
-    });
+export async function deleteSymbol (symbol: string) {
+  await withAutoreleaseConnection(async connection => {
+    await connection.query(`DELETE
+                            FROM user_selected_symbol
+                            WHERE stock_symbol = ?`, [symbol]);
+    await connection.query(`DELETE
+                            FROM user_selected_stocks
+                            WHERE symbol = ?`, [symbol]);
+  });
 }
 
-function getOldestSymbol(): Promise<Symbol> {
-    return new Promise((resolve, reject) => {
-      getConnection().then(connection => {
-        connection.query(
-            `SELECT * FROM user_selected_symbol ORDER BY updated_date LIMIT 1`
-        ).catch(err => {
-          console.error(err);
-          reject(err);
-        }).then((results) => {
-          let rows: RowDataPacket[] = (results as RowDataPacket[][])[0];
-          let symbol = rows.map(row => ({
-                symbol: row.stock_symbol,
-                createdDate: new Date(Date.parse(row.created_date)),
-                updatedDate: new Date(Date.parse(row.updated_date))
-            })
-          );
-          if (!symbol || symbol.length == 0) {
-            reject("can not get oldest symbol");
-          }
-          resolve(symbol[0]);
-        });
-      })
-    });
+async function getOldestSymbol (connection: Connection): Promise<Symbol> {
+  const results = await connection.query(
+    `SELECT *
+     FROM user_selected_symbol
+     ORDER BY updated_date
+     LIMIT 1`,
+  );
+  const rows: RowDataPacket[] = (results as RowDataPacket[][])[0];
+  if (!rows || rows.length == 0) {
+    throw new Error('can not get oldest symbol');
+  }
+  const symbol = rows.map(row => ({
+      symbol: row.stock_symbol,
+      createdDate: new Date(Date.parse(row.created_date)),
+      updatedDate: new Date(Date.parse(row.updated_date)),
+    }),
+  );
+  return symbol[0];
 }
 
-function getNearestUpdateTimeDiff(): Promise<Number> {
-    return new Promise((resolve, reject) => {
-      getConnection().then(connection => {
-        connection.query(
-            `SELECT NOW() as now, updated_date as nearest
-             FROM user_selected_symbol
-             ORDER BY updated_date DESC
-             LIMIT 1`
-        ).catch(err => {
-          console.error(err);
-          reject(err);
-        }).then((results) => {
-          let rows: RowDataPacket[] = (results as RowDataPacket[][])[0];
-          let timediff = rows.map(row =>
-            Date.parse(row.now) - Date.parse(row.nearest)
-          );
-          if (!timediff || timediff.length == 0) {
-            reject("can not get newest symbol");
-          }
-          resolve(timediff[0]);
-        });
-      })
-    });
+async function getNearestUpdateTimeDiff (connection: Connection): Promise<Number> {
+  const results = await connection.query(
+    `SELECT NOW() AS now, updated_date AS nearest
+     FROM user_selected_symbol
+     ORDER BY updated_date DESC
+     LIMIT 1`,
+  );
+  const rows: RowDataPacket[] = (results as RowDataPacket[][])[0];
+  const timediff = rows.map(row =>
+    Date.parse(row.now) - Date.parse(row.nearest),
+  );
+  if (!timediff || timediff.length == 0) {
+    throw new Error('can not get newest symbol');
+  }
+  return timediff[0];
 }
 
-export function updateOldestSymbol(): Promise<Symbol> {
-    return new Promise((resolve, reject) => {
-        // if nearest updated timediff (ms) less than 20s, do not update
-        getNearestUpdateTimeDiff().then(timediff => {
-            console.log(timediff);
+export function updateOldestSymbol (): Promise<Symbol> {
+  return withAutoreleaseConnection(async connection => {
+    const timediff = await getNearestUpdateTimeDiff(connection);
+    console.log(timediff);
+    if (timediff < 1000 * 20) {
+      throw new Error('nearest updated timediff less than 20s');
+    }
 
-            if (timediff < 1000 * 20) {
-                reject("nearest updated timediff less than 20s");
-                return;
-            }
-        });
-
-        getOldestSymbol().then(oldestSymbol => {
-            getConnection().then(connection => {
-                connection.query(
-                    `UPDATE user_selected_symbol SET updated_date = NOW() WHERE stock_symbol = ?`, [oldestSymbol.symbol]
-                ).then(() => {
-                    resolve(oldestSymbol);
-                }).catch(err => {
-                    console.error(err);
-                    reject(err);
-                });
-            });
-        });
-    });
+    const oldestSymbol = await getOldestSymbol(connection);
+    await connection.query(
+      `UPDATE user_selected_symbol
+       SET updated_date = NOW()
+       WHERE stock_symbol = ?`, [oldestSymbol.symbol],
+    );
+    return oldestSymbol;
+  });
 }
