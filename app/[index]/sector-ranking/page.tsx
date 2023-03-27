744e76d832fbc0cdd2a5d053c42382d01a73b004
Jagger <634750802@qq.com>
2023-03-27 10:30:28 +0800

    update text

--- app/[index]/sector-ranking/page.tsx
+++ app/[index]/sector-ranking/page.tsx
@@ -5,9 +5,10 @@ export default function Page ({ params: { index } }: { params: { index: string }
     <section className="mt-8">
       <h2 id="industry-ranking">Ranking</h2>
       <p className="mt-4">
-      The following chart shows the industry ranking of S&P 500 constituent stocks based on market capitalization. 
-      In addition to market capitalization, we also display the number of companies and average revenue growth rate for each industry in the chart.<br>
-      we also provide a table format at the bottom, which is useful for a more intuitive view of the various industry metrics.
+        The following chart shows the industry ranking of S&P 500 constituent stocks based on market capitalization.
+        In addition to market capitalization, we also display the number of companies and average revenue growth rate for each industry in the chart.
+        <br />
+        we also provide a table format at the bottom, which is useful for a more intuitive view of the various industry metrics.
       </p>
       <SectorsRanking index={index} />
     </section>
--- app/optional/page.tsx
+++ app/optional/page.tsx
@@ -18,6 +18,10 @@ const Page = () => {
 
   return (
     <>
+      <h2>Selected stocks</h2>
+      <p className='my-4'>
+        On this page, you can add the stocks you want to follow by clicking the '+' button. Clicking on the name of a stock will take you to its detailed page, where you can see its performance over time.
+      </p>
       <StocksTable stocks={stocks} onInfoLoad={handleInfoLoad} onChange={setStocks} />
     </>
   );
